9530f52bcf157b6a3eae031e5dd6247181406053
Cuberian <themess@inbox.ru>
2021-10-10 16:49:29 +0500

    set new package

--- src/app.module.ts
+++ src/app.module.ts
@@ -46,12 +46,12 @@ const productionDbConfig: any = {
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     SequelizeModule.forRoot({
+      dialect: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
       username: process.env.POSTGRES_USER,
       password: String(process.env.POSTGRES_PASSWORD),
       database: process.env.POSTGRES_DB,
-      dialect: 'postgres',
       dialectOptions: {
         ssl: {
           require: true,
